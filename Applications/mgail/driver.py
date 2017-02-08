Solved gradient bug with ALR. Modified Mujoco to start at random points at the episode

Applications/mgail/driver.py
@@ -34,6 +34,8 @@ class DRIVER(object):
         self.itr = 0
         self.best_reward = 0
         self.mode = 'SL'
+        self.a_mean = 0.
+        self.a_std = 0.
         np.set_printoptions(precision=2)
         np.set_printoptions(linewidth=220)
 
@@ -72,10 +74,6 @@ class DRIVER(object):
     def train_transition(self):
         alg = self.algorithm
         for k_t in range(self.env.K_T):
-            # trans_iters = self.num_transition_iters()
-            # state_, action = self.algorithm.er_agent.sample_trajectory(trans_iters)
-            # states = np.transpose(state_, axes=[1, 0, 2])
-            # actions = np.transpose(action, axes=[1, 0, 2])
             states_, actions, _, states, terminals = self.algorithm.er_agent.sample()
             states_ = np.squeeze(states_, axis=1)
             states = np.squeeze(states, axis=1)
@@ -90,16 +88,20 @@ class DRIVER(object):
     def train_discriminator(self):
         alg = self.algorithm
         for k_d in range(self.env.K_D):
-            state_a_, action_a, _, _, _ = self.algorithm.er_agent.sample()
-            state_e_, action_e, _, _, _ = self.algorithm.er_expert.sample()
-            state_a_ = np.squeeze(state_a_, axis=1)
+            if self.env.dad_gan:
+                state_a_, action_a = self.batch
+            else:
+                state_a_, action_a = self.algorithm.er_agent.sample()[:2]
+                state_a_ = np.squeeze(state_a_, axis=1)
+
+            state_e_, action_e = self.algorithm.er_expert.sample()[:2]
             state_e_ = np.squeeze(state_e_, axis=1)
             states = np.concatenate([state_a_, state_e_])
             actions = np.concatenate([action_a, action_e])
 
             # labels (policy/expert) : 0/1, and in 1-hot form: policy-[1,0], expert-[0,1]
-            labels_a = np.zeros(shape=(alg.er_agent.batch_size,))
-            labels_e = np.ones(shape=(alg.er_agent.batch_size,))
+            labels_a = np.zeros(shape=(state_a_.shape[0],))
+            labels_e = np.ones(shape=(state_e_.shape[0],))
             labels = np.expand_dims(np.concatenate([labels_a, labels_e]), axis=1)
             fetches = [alg.discriminator.minimize, alg.discriminator.loss, alg.discriminator.mean_abs_grad,
                        alg.discriminator.mean_abs_w, alg.discriminator.acc]
@@ -118,10 +120,9 @@ class DRIVER(object):
             # reset the policy gradient
             self.sess.run([alg.policy.reset_grad_op], {})
 
-            state_e_, action_e, _, _, _ = self.algorithm.er_expert.sample()
-            state_e_ = np.squeeze(state_e_, axis=1)
-
             if mode == 'SL':
+                state_e_, action_e = self.algorithm.er_expert.sample()[:2]
+                state_e_ = np.squeeze(state_e_, axis=1)
                 # accumulate the SL gradient
                 fetches = [alg.policy.accum_grads_sl, alg.policy.loss_sl]
                 feed_dict = {alg.states: state_e_, alg.actions: action_e, alg.do_keep_prob: self.env.do_keep_prob}
@@ -140,11 +141,11 @@ class DRIVER(object):
                 self.sess.run([alg.policy_.copy_weights_op], {})
 
             else:  # Adversarial Learning
-                if self.env.get_status():
-                    state = self.env.reset()
-                    self.episode_noise_shift = np.random.normal(scale=alg.env.sigma)
-                else:
-                    state = self.env.get_state()
+                # if self.env.get_status():
+                #     state = self.env.reset()
+                #     self.episode_noise_shift = np.random.normal(scale=alg.env.sigma)
+                # else:
+                #     state = self.env.get_state()
 
                 # Accumulate the (noisy) adversarial gradient
                 # for i in range(self.env.policy_accum_steps):
@@ -156,31 +157,100 @@ class DRIVER(object):
                 #     run_vals = self.sess.run(fetches, feed_dict)
                 #     self.update_stats('policy', 'loss', run_vals[1])
 
+                # apply AL gradient
+                # self.sess.run([alg.policy.apply_grads_al], {})
+
+                # output gradient / weights statistics
+                # run_vals = self.sess.run([alg.policy.mean_abs_grad_al, alg.policy.mean_abs_w_al], {})
+                # self.update_stats('policy', 'grad', run_vals[0])
+                # self.update_stats('policy', 'weights', run_vals[1])
+
                 # Plain Adversarial Learning
+                # for i in range(self.env.policy_accum_steps):
+                if self.env.dad_gan:
+                    states_, _ = self.batch
+                else:
+                    states_ = self.algorithm.er_agent.sample()[0]
+                    states_ = np.squeeze(states_, axis=1)
+
                 fetches = [alg.policy.accum_grads_alr, alg.policy.loss_alr]
-                feed_dict = {alg.states: np.array(state_e_), alg.do_keep_prob: 1.}
+                feed_dict = {alg.states: np.array(states_), alg.do_keep_prob: self.env.do_keep_prob}
                 run_vals = self.sess.run(fetches, feed_dict)
                 self.update_stats('policy', 'loss', run_vals[1])
 
-                # Temporal Regularization
-                # TODO: disabled dropout for tr loss
-                self.sess.run([alg.policy.accum_grads_tr], {alg.states: state_e_, alg.do_keep_prob: 1.})
-
-                # copy weights: w_policy_ <- w_policy
-                self.sess.run([alg.policy_.copy_weights_op], {})
-
-                # apply AL gradient
-                self.sess.run([alg.policy.apply_grads_al], {})
+                # apply ALR gradient
+                self.sess.run([alg.policy.apply_grads_alr], {})
 
                 # output gradient / weights statistics
-                run_vals = self.sess.run([alg.policy.mean_abs_grad_al, alg.policy.mean_abs_w_al], {})
+                run_vals = self.sess.run([alg.policy.mean_abs_grad_alr, alg.policy.mean_abs_w_alr], {})
                 self.update_stats('policy', 'grad', run_vals[0])
                 self.update_stats('policy', 'weights', run_vals[1])
 
-    def collect_experience(self, record=1, vis=0, n_steps=None, noise_flag=True):
+                # # Temporal Regularization
+                # self.sess.run([alg.policy.accum_grads_tr], {alg.states: states_, alg.do_keep_prob: 1.})
+                # self.sess.run([alg.policy.apply_grads_tr], {})
+                #
+                # # copy weights: w_policy_ <- w_policy
+                # self.sess.run([alg.policy_.copy_weights_op], {})
+
+    def create_agent_batch(self):
+        states_ = []
+        actions = []
+        n = 0
+        while n < self.env.batch_size:
+            states_n_actions = self.play_agent()
+            states_ += states_n_actions[0]
+            actions += states_n_actions[1]
+            n += len(states_n_actions[0])
+
+        p = np.random.choice(n, self.env.batch_size)
+        self.batch = [np.asarray(states_)[p], np.asarray(actions)[p]]
+
+    def play_agent(self, n_steps=2, start_at_zero=False):
+
+        if start_at_zero:
+            obs_ = self.env.reset()
+        else:
+            qposs, qvels = self.algorithm.er_expert.sample()[5:]
+            obs_ = self.env.reset(qpos=qposs[0], qvel=qvels[0])
+
+        states_ = []
+        actions = []
+
         alg = self.algorithm
-        observation = self.env.reset()
-        self.episode_noise_shift = np.random.normal(scale=alg.env.sigma)
+
+        if n_steps is None:
+            n_steps = self.env.n_steps_test
+
+        t = 0
+        done = False
+        while not done:
+            # a = self.sess.run(fetches=[alg.action_test], feed_dict={alg.states: np.reshape(obs_, [1, -1]), alg.do_keep_prob: 1.})
+            a = self.sess.run(fetches=[alg.action_test], feed_dict={alg.states: np.reshape(obs_, [1, -1]),
+                                                                    alg.do_keep_prob: 1.,
+                                                                    alg.noise: False,
+                                                                    alg.noise_mean: self.episode_noise_shift,
+                                                                    alg.temp: self.env.temp})
+
+            obs, reward, done, info, qpos, qvel = self.env.step(a, mode='python')
+            states_.append(obs_)
+            actions.append(a[0])
+            obs_ = obs
+            done = done or t > n_steps
+            t += 1
+        return states_, actions
+
+    def collect_experience(self, record=1, vis=0, n_steps=None, noise_flag=True, start_at_zero=True):
+        alg = self.algorithm
+
+        if start_at_zero:
+            observation = self.env.reset()
+        else:
+            qposs, qvels = self.algorithm.er_expert.sample()[5:]
+            observation = self.env.reset(qpos=qposs[0], qvel=qvels[0])
+
+        self.episode_noise_shift = 0 * np.random.normal(scale=alg.env.sigma)
+
         do_keep_prob = self.env.do_keep_prob
         t = 0
         R = 0
@@ -201,7 +271,7 @@ class DRIVER(object):
                                                                     alg.noise_mean: self.episode_noise_shift,
                                                                     alg.temp: self.env.temp})
 
-            observation, reward, done, info = self.env.step(a, mode='python')
+            observation, reward, done, info, qpos, qvel = self.env.step(a, mode='python')
 
             done = done or t > n_steps
 
@@ -209,13 +279,18 @@ class DRIVER(object):
 
             R += reward
 
+            self.a_mean = a[0].mean()
+            self.a_std = a[0].std()
+
             if record:
                 if self.env.continuous_actions:
                     action = a
                 else:
                     action = np.zeros((1, self.env.action_size))
                     action[0, a[0]] = 1
-                self.algorithm.er_agent.add(actions=action, rewards=[reward], next_states=[observation], terminals=[done])
+                self.algorithm.er_agent.add(actions=action, rewards=[reward], next_states=[observation], terminals=[done], qposs=[qpos], qvels=[qvel])
+
+        self.avg_policy_time = t
 
         return R
 
@@ -261,37 +336,41 @@ class DRIVER(object):
             if self.env.train_flags[2]:
                 self.mode = 'Prep'
                 if self.itr < (self.env.model_identification_time + self.env.prep_time):
+                    if self.env.dad_gan:
+                        self.create_agent_batch()
                     self.train_discriminator()
                 else:
                     self.mode = 'AL'
-                    if self.discriminator_policy_switch:
+                    if self.env.dad_gan:
+                        self.create_agent_batch()
+                        self.train_discriminator()
+                        self.train_policy(self.mode)
                         self.train_discriminator()
                     else:
-                        self.train_policy(self.mode)
+                        if self.discriminator_policy_switch:
+                            self.train_discriminator()
+                        else:
+                            self.train_policy(self.mode)
+                            pass
 
                         if self.itr % self.env.collect_experience_interval == 0:
-                            self.collect_experience()
+                            self.collect_experience(start_at_zero=False, n_steps=self.env.n_steps_train)
 
-                    # switch discriminator-policy
-                    if self.itr % self.env.discr_policy_itrvl == 0:
-                        self.discriminator_policy_switch = not self.discriminator_policy_switch
+                        # switch discriminator-policy
+                        if self.itr % self.env.discr_policy_itrvl == 0:
+                            self.discriminator_policy_switch = not self.discriminator_policy_switch
 
         # print progress
         if self.itr % 100 == 0:
             self.print_info_line('slim')
 
-    # def num_transition_iters(self):
-    #     if self.loss[0] > self.env.trans_loss_th:
-    #         trans_iters = 2
-    #     else:
-    #         trans_iters = int((2.-self.env.max_trans_iters)/self.env.trans_loss_th * self.loss[0] + self.env.max_trans_iters)
-    #     return trans_iters
-
     def print_info_line(self, mode):
         if mode == 'full':
-            buf = '%s Training(%s): iter %d, loss: %s, disc_acc: %f, grads: %s, weights: %s, er_count: %d, R: %.1f, R_std: %.2f\n' % \
-                  (time.strftime("%H:%M:%S"), self.mode, self.itr, self.loss, self.disc_acc, self.abs_grad, self.abs_w,
-                   self.algorithm.er_agent.count, self.reward, self.reward_std)
+            buf = '%s Training(%s): iter %d, loss: %s, disc_acc: %.2f, grads: [%.2f, %.2f, %.4f], ' \
+                  'weights: %s, er_count: %d, R: %.1f, R_std: %.2f, a_mean: %.2f, a_std: %.2f\n' % \
+                  (time.strftime("%H:%M:%S"), self.mode, self.itr, self.loss, self.disc_acc,
+                   self.abs_grad[0], self.abs_grad[1], self.abs_grad[2], self.abs_w,
+                   self.algorithm.er_agent.count, self.reward, self.reward_std, self.a_mean, self.a_std)
             if hasattr(self.env, 'log_fid'):
                 self.env.log_fid.write(buf)
         else:

Applications/mgail/environments/humanoid/environment.py
@@ -19,34 +19,23 @@ class ENVIRONMENT(object):
     def _step(self, a):
         a = np.squeeze(a)
         self.t += 1
-        state_ = self.state
-        self.state, self.reward, self.done, self.info = self.gym.step(a)
+        self.state, self.reward, self.done, self.info, self.qpos, self.qvel = self.gym.step(a)
 
-        e = np.sqrt(((state_ - self.state) ** 2).sum())
-
-        # if np.any(self.state[self.inactive_state_features] != 0):
-        #     # print 'Not stable'
-        #     self.done = True
-
-        # if e < 0.05:
-        #     # print 'Freeze'
-        #     self.done = True
-        return self.state.astype(np.float32), self.reward.astype(np.float32), self.done
+        return self.state.astype(np.float32), self.reward.astype(np.float32), self.done, self.qpos.astype(np.float32), self.qvel.astype(np.float32)
 
     def step(self, a, mode):
         if mode == 'tensorflow':
-            state, reward, done = tf.py_func(self._step, inp=[a], Tout=[tf.float32, tf.float32, tf.bool], name='env_step_func')
-            # DEBUG: flatten state. not sure if correctly
+            state, reward, done, qpos, qvel = tf.py_func(self._step, inp=[a], Tout=[tf.float32, tf.float32, tf.bool, tf.float32, tf.float32], name='env_step_func')
             state = tf.reshape(state, shape=(self.state_size,))
             done.set_shape(())
         else:
-            state, reward, done = self._step(a)
+            state, reward, done, qpos, qvel = self._step(a)
         info = 0.
-        return state, reward, done, info
+        return state, reward, done, info, qpos, qvel
 
-    def reset(self):
+    def reset(self, qpos=None, qvel=None):
         self.t = 0
-        self.state = self.gym.reset()
+        self.state = self.gym.reset(qpos, qvel)
         return self.state
 
     def get_status(self):
@@ -62,62 +51,62 @@ class ENVIRONMENT(object):
         self.state_size = self.gym.observation_space.shape[0]
         self.action_size = self.gym.action_space.shape[0]
         self.action_space = np.asarray([None]*self.action_size)
+        self.qpos_size = self.gym.env.model.data.qpos.shape[0]
+        self.qvel_size = self.gym.env.model.data.qvel.shape[0]
 
     def _train_params(self):
-        self.trained_model = None  #'/home/nir/work/git/Buffe/Applications/mgail/environments/humanoid/snapshots/2017-01-08-21-08-403000.sn'
+        self.trained_model = None
         self.train_mode = True
         self.train_flags = [0, 1, 1]  # [autoencoder, transition, discriminator/policy]
-        self.expert_data = 'expert_data/er-2017-01-03-14-50-240T-sorted.bin'
-        self.disc_as_classifier = True
-        self.pre_load_buffer = True
+        self.expert_data = 'expert_data/er-2017-02-06-15-06-490k-fake.bin'
+        self.pre_load_buffer = False
         self.n_train_iters = 1000000
-        self.n_episodes_test = 1
-        self.kill_itr = self.n_train_iters
-        self.reward_kill_th = -1
+        self.n_episodes_test = 5
         self.test_interval = 1000
         self.n_steps_test = 1000
+        self.good_reward = 8000
         self.vis_flag = False
         self.save_models = False
+        self.save_agent_er = False
+        self.save_agent_at_itr = 50000
         self.config_dir = None
         self.continuous_actions = True
-        self.success_th = 4500
         self.weight_decay = 1e-7
+        self.al_loss = 'CE'
+        self.dad_gan = False
 
         # Main parameters to play with:
-        self.er_agent_size = 50000
-        self.reset_itrvl = 10000
-        self.n_reset_iters = 10000
-        self.model_identification_time = 50000
-        self.prep_time = 10000
-        self.collect_experience_interval = 5
-        self.n_steps_train = 2
+        self.er_agent_size = 200000
+        self.model_identification_time = 1000
+        self.prep_time = 1000
+        self.collect_experience_interval = 15
+        self.n_steps_train = 10
         self.discr_policy_itrvl = 100
-        self.K_T = 2
+        self.K_T = 0
         self.K_D = 1
         self.K_P = 1
         self.gamma = 0.99
         self.batch_size = 70
         self.policy_al_w = 1e-2
         self.policy_tr_w = 5e-3
-        self.policy_accum_steps = 3
-        self.total_trans_err_allowed = 100000# 1e-0
-        # self.trans_loss_th = 50
+        self.policy_accum_steps = 5
+        self.total_trans_err_allowed = 1.
         self.max_trans_iters = 2
         self.temp = 1.
         self.cost_sensitive_weight = 1.1
 
         self.t_size = [500, 300, 200]
-        self.d_size = [800, 400]
-        self.p_size = [600, 300]
+        self.d_size = [1000, 500, 150]
+        self.p_size = [100, 50]
 
-        self.t_lr = 0.005
+        self.t_lr = 1e-2
         self.d_lr = 0.001
         self.p_lr = 0.0001
 
         self.w_std = 0.15
 
         self.noise_intensity = 6.
-        self.do_keep_prob = 0.85
+        self.do_keep_prob = 0.75
 
         self.smooth_over_steps = 50
         self.fm_lr = 1e-2

Applications/mgail/mgail.py
@@ -27,11 +27,11 @@ class MGAIL(object):
         transformed_state_size = self.env.state_size
 
         self.discriminator = __import__('discriminator').DISCRIMINATOR(in_dim=transformed_state_size + self.env.action_size,
-                                                                       out_dim=1+1*self.env.disc_as_classifier,
+                                                                       out_dim=2,
                                                                        size=self.env.d_size,
                                                                        lr=self.env.d_lr,
                                                                        do_keep_prob=self.do_keep_prob,
-                                                                       weight_decay=self.env.weight_decay)
+                                                                       weight_decay=1e-4)#self.env.weight_decay)
 
         self.policy = __import__('policy').POLICY(in_dim=transformed_state_size,
                                                   out_dim=self.env.action_size,
@@ -55,6 +55,8 @@ class MGAIL(object):
                            state_dim=self.env.state_size,
                            action_dim=self.env.action_size,
                            reward_dim=1,  # stub connection
+                           qpos_dim=self.env.qpos_size,
+                           qvel_dim=self.env.qvel_size,
                            batch_size=self.env.batch_size,
                            history_length=1)
 
@@ -96,7 +98,7 @@ class MGAIL(object):
             return state_a
 
         states_a = forward_model_loop(states_, actions)
-        fw_model_loss = tf.nn.l2_loss(states - states_a)
+        fw_model_loss = tf.reduce_mean(tf.square(states - states_a))
         self.forward_model.train(objective=fw_model_loss)
 
         # # TODO : check pretrain loss on different embedding_dim dize
@@ -136,9 +138,7 @@ class MGAIL(object):
         # 4. Policy
         # 4.1 SL
         actions_a = self.policy.forward(states, autoencoder)
-        policy_sl_loss = tf.reduce_mean(tf.abs(actions_a - actions))  # action == expert action
-        # TODO: checking euclidean loss
-        policy_sl_loss = tf.nn.l2_loss(1 * (actions_a - actions))  # action == expert action
+        policy_sl_loss = tf.nn.l2_loss(actions_a - actions)  # action == expert action
         self.policy.train(objective=policy_sl_loss, mode='sl')
         self.policy.loss_sl_summary = tf.scalar_summary('loss_p_sl', self.policy.loss_sl)
 
@@ -179,7 +179,7 @@ class MGAIL(object):
             else:
                 a_sim = tf.argmax(a, dimension=1)
 
-            state_env, reward, env_term_sig, info = self.env.step(a_sim, mode='tensorflow')
+            state_env, _, env_term_sig, = self.env.step(a_sim, mode='tensorflow')[:3]
             state_e = common.normalize(state_env, self.er_expert.states_mean, self.er_expert.states_std)
             state_e = tf.stop_gradient(state_e)
 
@@ -213,5 +213,18 @@ class MGAIL(object):
     def al_loss(self, d):
         logit_agent, logit_expert = tf.split(split_dim=1, num_split=2, value=d)
         logit_gap = logit_agent - logit_expert
-        loss = tf.nn.l2_loss(tf.mul(logit_gap, self.env.policy_al_w * tf.to_float(logit_gap > 0)))
+        valid_cond = tf.stop_gradient(tf.to_float(logit_gap > 0))
+        valid_gaps = tf.mul(logit_gap, valid_cond)
+
+        # L2
+        if self.env.al_loss == 'L2':
+            loss = tf.nn.l2_loss(tf.mul(logit_gap, self.env.policy_al_w * tf.to_float(logit_gap > 0)))
+        # L1
+        elif self.env.al_loss == 'L1':
+            loss = tf.reduce_mean(valid_gaps)
+        # Cross entropy
+        elif self.env.al_loss == 'CE':
+            labels = tf.concat(1, [tf.zeros_like(logit_agent), tf.ones_like(logit_expert)])
+            d_cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=d, labels=labels)
+            loss = self.env.policy_al_w * tf.reduce_mean(d_cross_entropy)
         return loss

utils/common.py
@@ -4,6 +4,7 @@ import time
 import tensorflow as tf
 import numpy as np
 import os
+import sys
 
 def get_params(obj):
     params = {}
@@ -107,32 +108,40 @@ def multivariate_pdf_np(x, mu, sigma):
     return p_x
 
 
-def save_er(module, directory):
+def save_er(module, directory, exit_=False):
     fname = directory + '/er' + time.strftime("-%Y-%m-%d-%H-%M") + '.bin'
     f = file(fname, 'wb')
     cPickle.dump(module, f)
     print 'saved ER: %s' % fname
+    if exit_:
+        sys.exit(0)
 
 
 def load_er(fname, batch_size, history_length, traj_length):
     f = file(fname, 'rb')
-    object = cPickle.load(f)
-    object.batch_size = batch_size
-    state_dim = object.states.shape[-1]
-    action_dim = object.actions.shape[-1]
-    object.prestates = np.empty((batch_size, history_length, state_dim), dtype=np.float32)
-    object.poststates = np.empty((batch_size, history_length, state_dim), dtype=np.float32)
-    object.traj_states = np.empty((batch_size, traj_length, state_dim), dtype=np.float32)
-    object.traj_actions = np.empty((batch_size, traj_length-1, action_dim), dtype=np.float32)
-    object.states_min = np.min(object.states[:object.count], axis=0)
-    object.states_max = np.max(object.states[:object.count], axis=0)
-    # object.states_mean = np.mean(object.states, axis=0)
-    # object.states_std = np.std(object.states, axis=0)
-    object.actions_min = np.min(object.actions[:object.count], axis=0)
-    object.actions_max = np.max(object.actions[:object.count], axis=0)
-    # object.actions_mean = np.mean(object.actions, axis=0)
-    # object.actions_std = np.std(object.actions, axis=0)
-    return object
+    er = cPickle.load(f)
+    er.batch_size = batch_size
+    er = set_er_stats(er, history_length, traj_length)
+    return er
+
+
+def set_er_stats(er, history_length, traj_length):
+    state_dim = er.states.shape[-1]
+    action_dim = er.actions.shape[-1]
+    er.prestates = np.empty((er.batch_size, history_length, state_dim), dtype=np.float32)
+    er.poststates = np.empty((er.batch_size, history_length, state_dim), dtype=np.float32)
+    er.traj_states = np.empty((er.batch_size, traj_length, state_dim), dtype=np.float32)
+    er.traj_actions = np.empty((er.batch_size, traj_length-1, action_dim), dtype=np.float32)
+    er.states_min = np.min(er.states[:er.count], axis=0)
+    er.states_max = np.max(er.states[:er.count], axis=0)
+    er.actions_min = np.min(er.actions[:er.count], axis=0)
+    er.actions_max = np.max(er.actions[:er.count], axis=0)
+    er.states_mean = np.mean(er.states[:er.count], axis=0)
+    er.actions_mean = np.mean(er.actions[:er.count], axis=0)
+    er.states_std = np.std(er.states[:er.count], axis=0)
+    er.states_std[er.states_std == 0] = 1
+    er.actions_std = np.std(er.actions[:er.count], axis=0)
+    return er
 
 
 def scalar_to_4D(x):